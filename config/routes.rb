8a86c19c0142b8989d0cd467985b03ecf0208e30
Suguru Odai <ourqwers@gmail.com>
2014-04-04 18:31:44 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ RailsProject::Application.routes.draw do
 
   resources :microposts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
